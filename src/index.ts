41fdfb959c4c143ff70a4396040656479e3ffc97
JonayKB <jonaykb@gmail.com>
2025-12-04 22:20:55 +0000

    LETS START THE PARTYYY

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { loadConfig } from './utils/configManager';
 import updateConfirmationMessage from './utils/updateMessage';
 import sendConfirmationModal from "./utils/sendConfirmateModal";
 import cron from 'node-cron';
+import { set } from "pm2";
 
 config.config();
 
@@ -32,7 +33,29 @@ const client = new Client({
     ],
     partials: [Partials.Message, Partials.Channel, Partials.Reaction]
 });
+async function sendConfirmationModalToMembers() {
+    try {
+        const allMembers = await client.guilds.cache.first()?.members.fetch();
+        if (!allMembers) {
+            console.warn('⚠️ No members found in the guild.');
+            return;
+        }
+        const membersToSend = allMembers.filter(member =>
+            !member.user.bot &&
+            !member.roles.cache.has(ROL_NOT_TO_SEND_ID)
+        ).map(member => member.user);
 
+        console.log(`📊 Members to send confirmation modal: ${membersToSend.length}`);
+        await sendConfirmationModal(membersToSend);
+        console.info('✅ Confirmation modal sent by cron job');
+    } catch (err: any) {
+        if (err.data.opcode === 8) {
+            setTimeout(sendConfirmationModalToMembers, err.data.retry_after * 1000);
+            return;
+        }
+        console.error('❌ Error sending confirmation modal in cron job:', err);
+    }
+}
 // Registrar comandos
 loadCommands(client);
 
@@ -56,20 +79,7 @@ client.once('clientReady', async () => {
     try {
         const timezone = process.env.TIMEZONE ?? 'Europe/Madrid'; // optional, set e.g. 'Europe/Madrid'
         const options = timezone ? { scheduled: true, timezone } : { scheduled: true };
-        cron.schedule('0 18 */3 * *', async () => {
-            try {
-                const admin = await client.users.fetch(ADMIN_ID);
-                if (!admin) {
-                    console.warn('⚠️ Admin user not found.');
-                    return;
-                }
-
-                await sendConfirmationModal([admin]);
-                console.info('✅ Confirmation modal sent by cron job');
-            } catch (err) {
-                console.error('❌ Error sending confirmation modal in cron job:', err);
-            }
-        }, options);
+        cron.schedule('0 18 */3 * *', sendConfirmationModalToMembers, options);
 
         console.info('⏰ Confirmation modal cron scheduled: every 3 days at 18:00');
     } catch (err) {
