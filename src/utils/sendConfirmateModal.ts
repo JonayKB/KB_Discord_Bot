2233ba6c71b75844db61c228f7944002c93f2abf
Jonay Contreras <88539815+JonayKB@users.noreply.github.com>
2025-12-06 00:42:07 +0000

    Update log message to use displayName

--- src/utils/sendConfirmateModal.ts
+++ src/utils/sendConfirmateModal.ts
@@ -36,7 +36,7 @@ export default async function sendConfirmationModal(users: User[] | GuildMember[
                 embeds: [embed],
                 components: [button]
             });
-            console.log(`Message sent to ${user.username}`);
+            console.log(`Message sent to ${user.displayName}`);
         } catch (e) {
             console.error(`❌ No se pudo enviar DM a ${user.displayName}`, e);
         }
