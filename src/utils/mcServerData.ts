554721464ee1c9f76babdbd9a34242a39be0ce71
JonayKB <jonaykb@gmail.com>
2025-12-08 19:39:15 +0000

    Added players

--- src/utils/mcServerData.ts
+++ src/utils/mcServerData.ts
@@ -1,4 +1,5 @@
 const { MINECRAFT_SERVER_IP, MINECRAFT_SERVER_PORT, MINECRAFT_API_PORT } = process.env;
+import { status } from 'minecraft-server-util';
 
 interface SparkRestResponse {
     tps_10s: number;
@@ -19,22 +20,32 @@ export default function fetchMcServerData() {
             }
             return response.json();
         })
-        .then((data: SparkRestResponse) => {
-            return {
-                tps_10s: data.tps_10s,
-                tps_1m: data.tps_1m,
-                tps_5m: data.tps_5m,
-                tps_15m: data.tps_15m,
-                mspt_1m: data.mspt_1m,
-                cpu: data.cpu,
-                playersOnline: 0,
-                maxPlayers: 0,
-                playerList: []
-            };
+        .then(async (data: SparkRestResponse) => {
+            const port = MINECRAFT_SERVER_PORT ? Number(MINECRAFT_SERVER_PORT) : 25565;
+            const response = await status(MINECRAFT_SERVER_IP ?? 'localhost', port, { timeout: 5000 }).then((status) => {
+                return {
+                    tps_10s: data.tps_10s,
+                    tps_1m: data.tps_1m,
+                    tps_5m: data.tps_5m,
+                    tps_15m: data.tps_15m,
+                    mspt_1m: data.mspt_1m,
+                    cpu: data.cpu,
+                    playersOnline: status.players.online,
+                    maxPlayers: status.players.max,
+                    playerList: status.players.sample ?? []
+                };
+            }).catch(() => {
+                return null;
+            });
+
+            return response;
+
+
+
+
 
         })
         .catch(error => {
-            console.warn('Error fetching Minecraft server data');
             return null;
         });
 
