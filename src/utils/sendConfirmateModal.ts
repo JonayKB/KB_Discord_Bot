ae3a0d9ed7a9b39fc7632fbb977984173e128133
JonayKB <jonaykb@gmail.com>
2026-06-02 17:07:14 +0100

    feat: added meesages again

--- src/utils/sendConfirmateModal.ts
+++ src/utils/sendConfirmateModal.ts
@@ -32,9 +32,9 @@ export default async function sendConfirmationModal(users: User[] | GuildMember[
                 .setColor('#3B82F6')
                 .setTitle('✨ Confirmación de Participación')
                 .setDescription(
-                    `Para unirte al servidor de **HYTALE**, completa el formulario usando el botón de abajo.
+                    `Para unirte al servidor de **MINECRAFT**, completa el formulario usando el botón de abajo.
 
-                    **Requisito:** Usuario de HYTALE.`
+                    **Requisito:** Usuario de MINECRAFT.`
                 )
                 .setTimestamp()
                 .setFooter({ text: 'Sistema automático de confirmación' });
